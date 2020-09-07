184562de5b5d61943a5d6eeffcbd0a424a417c9a
cjz.y <cjz.y@hotmail.com>
2020-09-07 22:54:57 +0800

    first ommit

---
+++ leetcode/287. 寻找重复数.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        s, f = nums[0], nums[nums[0]]
+        while s != f:
+            s = nums[s]
+            f = nums[nums[f]]
+
+        s = 0
+        while s != f:
+            s = nums[s]
+            f = nums[nums[f]]
+        return s
